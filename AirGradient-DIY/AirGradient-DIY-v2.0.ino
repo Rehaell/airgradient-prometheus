dfd08c59d38ee6dda1c88ae9ee2fe63739e87784
Rui Romao <github@ruiromao.pt>
2023-04-24 16:39:17 +0100

    modified:   AirGradient-DIY/AirGradient-DIY-v2.0.ino

--- AirGradient-DIY/AirGradient-DIY-v2.0.ino
+++ AirGradient-DIY/AirGradient-DIY-v2.0.ino
@@ -1,7 +1,15 @@
 /**
  * This sketch connects an AirGradient DIY sensor to a WiFi network, and runs a
  * tiny HTTP server to serve air quality metrics to Prometheus.
- */
+ * 
+ * This will be an upgrade to the existing code base and a complete re-write.
+ * The plan is to add the ability to adjust the time offset automatically as well as 
+ * access a web page allowing the user to adjust the sensors offset.
+ * 
+ **/
+
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
 
 
 
